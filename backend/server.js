0059545841d13dac8a99ee55e6eddade3a42ea07
Aftab <aftabsaifi2050@gmail.com>
2025-09-14 10:26:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
+
 
 
